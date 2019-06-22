69d147bfe4840630db03b39b35866af288381f39
M3 <Suresh.M3@spi-global.com>
2019-06-22 14:18:01 +0530

    kick start

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import './Login.css';
+import './Login.scss';
+import $ from 'jquery';
 
 class Login extends Component {
 
@@ -16,20 +17,75 @@ class Login extends Component {
   }
   handleSubmit = event => {
     event.preventDefault();
-    console.log(this.state);
+  }
+  toggleResetPswd = event => {
+	event.preventDefault();
+    $('#logreg-forms .form-signin').toggle() // display:block or none
+    $('#logreg-forms .form-reset').toggle() // display:block or none
+  }
+  toggleSignUp = event => {
+    event.preventDefault();
+    $('#logreg-forms .form-signin').toggle(); // display:block or none
+    $('#logreg-forms .form-signup').toggle(); // display:block or none
   }
 
   render() {
     return (
-      <div className="login-container" >
-        <form onSubmit={this.handleSubmit}>
-          <label> User ID: </label>
-          <input name="userid" type="text" value={this.state.userid} onChange={this.handleUserId} />
-          <label> Password: </label>
-          <input type="password" value={this.state.userpwd} onChange={this.handleUserPwd} />
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
+      <div id="logreg-forms">
+        <form className="form-signin">
+            <h1 className="h3 mb-3 font-weight-normal" style={{textAlign: "center"}}> Sign in</h1>
+            <div className="social-login">
+                <button className="btn facebook-btn social-btn" type="button"><span><i className="fab fa-facebook-f"></i> Sign in with Facebook</span> </button>
+                <button className="btn google-btn social-btn" type="button"><span><i className="fab fa-google-plus-g"></i> Sign in with Google+</span> </button>
+            </div>
+            <p style={{textAlign: "center"}}> OR  </p>
+            <div className="input-group">
+              <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required="" autoFocus="" value={this.state.userid} onChange={this.handleUserId}/>
+            </div>
+
+            <div className="input-group">
+              <input type="password" id="inputPassword" className="form-control" placeholder="Password" required=""  value={this.state.userpwd} onChange={this.handleUserPwd}/>
+            </div>
+
+            <div className="input-group">
+              <button className="btn btn-md btn-rounded btn-block form-control submit" type="submit"><i className="fas fa-sign-in-alt"></i> Sign in</button>
+            </div>
+
+            <a href="#" id="forgot_pswd" onClick={this.toggleResetPswd}>Forgot password?</a>
+            <hr />
+            {/* <!-- <p>Don't have an account!</p>  --> */}
+            <button className="btn btn-primary btn-block" type="button" id="btn-signup" onClick={this.toggleSignUp}><i className="fas fa-user-plus"></i> Sign up New Account</button>
+            </form>
+
+            <form action="#" className="form-reset">
+                <input type="email" id="resetEmail" className="form-control" placeholder="Email address" required="" autoFocus="" />
+                <button className="btn btn-primary btn-block" type="submit">Reset Password</button>
+                <a href="#" id="cancel_reset" onClick={this.toggleResetPswd}><i className="fas fa-angle-left"></i> Back</a>
+            </form>
+
+            <form action="#" className="form-signup">
+                <div className="social-login">
+                    <button className="btn facebook-btn social-btn" type="button"><span><i className="fab fa-facebook-f"></i> Sign up with Facebook</span> </button>
+                  </div>
+                  <div className="social-login">
+                    <button className="btn google-btn social-btn" type="button"><span><i className="fab fa-google-plus-g"></i> Sign up with Google+</span> </button>
+                </div>
+
+                <p style={{textAlign: "center"}}>OR</p>
+
+                <input type="text" id="user-name" className="form-control" placeholder="Full name" required="" autoFocus="" />
+                <input type="email" id="user-email" className="form-control" placeholder="Email address" required autoFocus="" />
+                <input type="password" id="user-pass" className="form-control" placeholder="Password" required autoFocus="" />
+                <input type="password" id="user-repeatpass" className="form-control" placeholder="Confirm Password" required autoFocus="" />
+
+				<div className="input-group">
+					<button className="btn btn-md btn-block submit" type="submit"><i className="fas fa-user-plus"></i> Sign Up</button>
+				</div>
+
+                <a href="#" id="cancel_signup" onClick={this.toggleSignUp}><i className="fa fa-angle-left"></i> Back</a>
+            </form>
+            <br />
+    </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.scss';
+
 import Login from './component/Login';
 import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(<Login />, document.getElementById('root'));
 
